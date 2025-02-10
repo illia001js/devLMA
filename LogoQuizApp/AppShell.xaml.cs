f0fb1a10df48b06e4a176ff7b4f11d235260ce66
Illia Olianovskyi <illia.olianovskyi@remira.com>
2025-02-10 08:35:37 +0100

    Template

---
+++ LogoQuizApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LogoQuizApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LogoQuizApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LogoQuizApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
